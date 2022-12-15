ebda98351bb52043238578186d0119a2f302fcee
Faith Mansilungan <faith0094@stud.kea.dk>
2022-12-15 11:37:11 +0100

    Oprettelse af page- og single.php

---
+++ wordpress/wp-content/themes/taioba-theme/page-gallery.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The site's entry point.
+ *
+ * Loads the relevant template part,
+ * the loop is executed (when needed) by the relevant template part.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+test please virk
+
+
+get_footer();
---
+++ wordpress/wp-content/themes/taioba-theme/single-tattoo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The site's entry point.
+ *
+ * Loads the relevant template part,
+ * the loop is executed (when needed) by the relevant template part.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+test single
+
+get_footer();
